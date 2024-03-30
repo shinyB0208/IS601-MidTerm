a5f2d7e0120bb24b4992e06f9ac5a28f1e55a974
shyyyyny <ka558@gmail.com>
2024-03-29 23:21:26 -0400

    app init.py fix

--- app/__init__.py
+++ app/__init__.py
@@ -4,11 +4,9 @@ import importlib
 import sys
 from app.command import CommandHandler, Command
 from dotenv import load_dotenv
-from app.logging_utility import LoggingUtility
 
 class App:
     def __init__(self):
-        LoggingUtility.configure_logging()
         load_dotenv()
         self.settings = self.load_environment_variables()
         self.settings.setdefault('ENVIRONMENT', 'PRODUCTION')
@@ -16,7 +14,7 @@ class App:
 
     def load_environment_variables(self):
         settings = {key: value for key, value in os.environ.items()}
-        LoggingUtility.info("Environment variables loaded.")
+        print("Environment variables loaded.")
         return settings
 
     def get_environment_variable(self, env_var: str = 'ENVIRONMENT'):
@@ -30,60 +28,45 @@ class App:
 
     def load_plugins(self):
         plugins_package = 'app.plugins'
-        calculation_path = os.path.join(plugins_package.replace('.', '/'), 'calculations')
-        history_path = os.path.join(plugins_package.replace('.', '/'), 'history')
-        other_plugins_path = plugins_package.replace('.', '/')
-        
-        self.load_plugin_commands(calculation_path, f'{plugins_package}.calculations')
-        self.load_plugin_commands(history_path, f'{plugins_package}.history')
-        
-        self.load_plugin_commands(other_plugins_path,f'{plugins_package}')
-               
-    def load_plugin_commands(self, path, package):
-        if not os.path.exists(path):
-            LoggingUtility.warning(f"Directory '{path}' not found.")
+        plugins_path = plugins_package.replace('.', '/')
+        if not os.path.exists(plugins_path):
+            print(f"Plugins directory '{plugins_path}' not found.")
             return
-        for _, plugin_name, _ in pkgutil.iter_modules([path]):
-            #"Easier to Ask for Forgiveness than Permission" (EAFP)
-            #Very few chance of getting the import error
-            try:
-                plugin_module = importlib.import_module(f'{package}.{plugin_name}')
-                command_instance = getattr(plugin_module, f'{plugin_name.capitalize()}Command')()
-                self.command_handler.register_command(plugin_name, command_instance)
-                LoggingUtility.info(f"Command '{plugin_name}' from plugin '{plugin_name}' registered.")
-            except ImportError as e:
-                LoggingUtility.error(f"Error importing plugin {plugin_name}: {e}")
-
-        
+        for _, plugin_name, is_pkg in pkgutil.iter_modules([plugins_path]):
+            if is_pkg:
+                try:
+                    plugin_module = importlib.import_module(f'{plugins_package}.{plugin_name}')
+                    self.register_plugin_commands(plugin_module, plugin_name)
+                except ImportError as e:
+                    print(f"Error importing plugin {plugin_name}: {e}")
 
     def register_plugin_commands(self, plugin_module, plugin_name):
         for item_name in dir(plugin_module):
             item = getattr(plugin_module, item_name)
             if isinstance(item, type) and issubclass(item, Command) and item is not Command:
+                # Command names are now explicitly set to the plugin's folder name
                 self.command_handler.register_command(plugin_name, item())
-                LoggingUtility.info(f"Command '{plugin_name}' from plugin '{plugin_name}' registered.")
+                print(f"Command '{plugin_name}' from plugin '{plugin_name}' registered.")
 
-     #initiates a Read-Eval-Print Loop (REPL), continuously accepting user input. 
     def start(self):
         self.load_plugins()
         self.show_menu()
-        LoggingUtility.info("Application started. Type 'exit' to exit.")
+        print("Application started. Type 'exit' to exit.")
         while True:
             input_line = input(">>> ").strip()
             if input_line == "":
-                continue
-            parts = input_line.split()
+                continue  # Skip empty input
+            parts = input_line.split()  # Split input into parts by whitespace
             command_name = parts[0]
-            args = parts[1:]
+            args = parts[1:]  # All the remaining parts are considered arguments
 
+        # Check if the command is 'menu' to show available commands
             if command_name == "menu":
                 self.show_menu()
                 continue
-            
-            #"Easier to Ask for Forgiveness than Permission" (EAFP)
-            #Very few chance of getting an unknown command error
+
             try:
                 self.command_handler.execute_command(command_name, *args)
             except KeyError:
-                LoggingUtility.error(f"Unknown command: {command_name}")
-                sys.exit(1)
+                print(f"Unknown command: {command_name}")
+                sys.exit(1)  # Use a non-zero exit code to indicate failure or incorrect command.
